cd487d10f66db605be485e9c4307f44632e882e3
lukebartlett97 <lukebartlett97@aol.co.uk>
2017-03-22 15:10:03 +0000

    GetAllMessages completed
    Woo 10% guys yay

--- src/msgServer/GetAllMsgsCommand.java
+++ src/msgServer/GetAllMsgsCommand.java
@@ -19,9 +19,41 @@ public class GetAllMsgsCommand implements Command
   public void execute() throws IOException
   {
 	//declare a variable user of type String and use it to get the user from the inputstream
-    
+    String user = in.readLine();
         //check if current user is not equal to null and current user is equal to the user (use the method getCurrentUser())
-      
+      String currentUser = conn.getCurrentUser();
+      if(currentUser != null){
+    	  if(currentUser.equals(user)){
+	    	  Message[] messages = null;
+	    	  messages = conn.getServer().getMessages().getAllMessages(user);
+	    	  if(messages != null){
+	    		  int responseCode = 200 + messages.length;
+	    		  out.write(Integer.toString(responseCode) + "\r\n");
+	    		  for(Message message : messages){
+	    			  out.write(message.getSender() + "\r\n");
+	    			  out.write(message.getDate() + "\r\n");
+	    			  out.write(message.getContent() + "\r\n");
+	    		  }
+	    		  out.flush();
+	    	  }
+	    	  else{
+	    		  out.write("200" + "\r\n");
+	        	  out.write("No Messages" + "\r\n");
+	    		  out.flush();
+	    	  }
+    	  }
+    	  else{
+        	  out.write("500" + "\r\n");
+        	  out.write("Incorrect User" + "\r\n");
+    		  out.flush();
+    		  
+    	  }
+      }
+      else{
+    	  out.write("500" + "\r\n");
+    	  out.write("You are not logged on" + "\r\n");
+		  out.flush();
+      }
         //intialise an array (msgs) that is used to hold all the messages read and set it's initialised value to null 
          
         //use the method getAllMessages(user) to populate the msgs array
@@ -36,7 +68,7 @@ public class GetAllMsgsCommand implements Command
           
         //Flush the outputstream
            
-        //capture adequet errors (No messages) or (You are not logged on)
+        //capture adequate errors (No messages) or (You are not logged on)
           
   }
 }
