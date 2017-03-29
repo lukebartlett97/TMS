fe18a925681b9254555eeb18a4bf452fa7c61769
lukebartlett97 <lukebartlett97@aol.co.uk>
2017-03-29 13:51:04 +0100

    Reminders
    Shell classes

---
+++ src/msgServer/AccessRemindersCommand.java
@@ -0,0 +1,23 @@
+package msgServer;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class AccessRemindersCommand implements Command  {
+	  private BufferedReader in;
+	  private BufferedWriter out;
+	  private MsgSvrConnection conn;
+
+	  public AccessRemindersCommand(BufferedReader in, BufferedWriter out, MsgSvrConnection serverConn)
+	  {
+	    this.in = in;
+	    this.out = out;
+	    this.conn = serverConn;
+	  }
+
+	  public void execute() throws IOException
+	  {
+		  ;
+	  }
+}
---
+++ src/msgServer/UpdateReminderCommand.java
@@ -0,0 +1,27 @@
+package msgServer;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class UpdateReminderCommand implements Command  {
+	  private BufferedReader in;
+	  private BufferedWriter out;
+	  private MsgSvrConnection conn;
+
+	  public UpdateReminderCommand(BufferedReader in, BufferedWriter out, MsgSvrConnection serverConn)
+	  {
+	    this.in = in;
+	    this.out = out;
+	    this.conn = serverConn;
+	  }
+	  public void updateReminder()
+	  {
+		  
+	  }
+
+	  public void execute() throws IOException
+	  {
+		  ;
+	  }
+}
