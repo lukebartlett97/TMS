3ab5525aef7efe99487f2da3e500eb7f1f22a89b
Philippa109 <philippalees22@yahoo.co.uk>
2017-04-05 15:47:29 +0100

    Update user using the database

--- src/msgServer/UpdateUserCommand.java
+++ src/msgServer/UpdateUserCommand.java
@@ -3,6 +3,7 @@ package msgServer;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.sql.*;
 
 public class UpdateUserCommand implements Command {
 	private BufferedReader in;
@@ -14,8 +15,28 @@ public class UpdateUserCommand implements Command {
 		this.out = out;
 		this.conn = serverConn;
 	}
+	
+	public void editDatabase(String username, String field, String value) {
+		if(value.equals("")){
+			value = null;
+		}
+		else{
+			value = "'" + value + "'";
+		}
+		try {
+			Connection dbConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/userdetails", "groupcwk", "textMessaging");
+			Statement updateStatement = dbConnection.createStatement();
+			String updateSql = "update customers "
+							+ " set " + field + "=" + value
+							+ " where username= '" + username + "'";
+			updateStatement.executeUpdate(updateSql);
+			System.out.println("Successfully updated database.");
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
 
-	public void editFile(String username, int field, String value) {
+	public void editFile(String username, String field, String value) {
 		//REMEMBER: When changing username, change current user
 		//When changing ANYTHING, update userInfo in MessageServer
 		System.out.println("TEST - Username:" + username + " FieldNo:" + field + " Value:" + value);
@@ -49,34 +70,37 @@ public class UpdateUserCommand implements Command {
 				(new ErrorCommand(in, out, conn, "That username already exists")).execute();
 				return;
 			}
-			editFile(username, 0, changeValue);
+			editDatabase(username, "username", changeValue);
 		} else if (changeVariable.equalsIgnoreCase("password")) {
-			if (changeVariable.length() <= 3) {
+			if (changeValue.length() < 3) {
 				(new ErrorCommand(in, out, conn, "Password too short")).execute();
 				return;
 			}
-			editFile(username, 1, changeValue);
+			editDatabase(username, "password", changeValue);
 		} else if (changeVariable.equalsIgnoreCase("dateofbirth")) {
-			if (changeVariable.equals("") == false) {
-				if (changeVariable.matches("([0-9]{2})/([0-9]{2})/([0-9]{4})") == false) {
-					(new ErrorCommand(in, out, conn, "please enter a valid date format dd/mm/yyyy")).execute();
+			if (changeValue.equals("") == false) {
+				if (changeValue.matches("([0-9]{4})-([0-9]{2})-([0-9]{2})") == false) {
+					(new ErrorCommand(in, out, conn, "please enter a valid date format yyyy-mm-dd")).execute();
 					return;
 				}
 			}
-			editFile(username, 2, changeValue);
+			editDatabase(username, "DOB", changeValue);
 		} else if (changeVariable.equalsIgnoreCase("phonenumber")) {
-			if (changeVariable.equals("") == false) {
-				if (changeVariable.length() != 11) {
+			if (changeValue.equals("") == false) {
+				if (changeValue.length() != 11) {
 					(new ErrorCommand(in, out, conn, "number must be 11 digits long")).execute();
 					return;
 				}
 			}
-			editFile(username, 3, changeValue);
+			editDatabase(username, "telNumber", changeValue);
 		} else if (changeVariable.equalsIgnoreCase("address")) {
-			editFile(username, 4, changeValue);
+			editDatabase(username, "address", changeValue);
 		}
 		else{
 			(new ErrorCommand(in, out, conn, "Valid types: username | password | dateofbirth | phonenumber | address")).execute();
+			return;
 		}
+		out.write("200 \r\n");
+		out.flush();
 	}
 }
